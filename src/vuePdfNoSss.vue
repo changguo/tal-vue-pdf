4a56855857afa3f6daac3ca00ddf0547cbb660e5
guochang <1149260569@qq.com>
2021-01-27 17:36:29 +0800

    修改worker

--- src/vuePdfNoSss.vue
+++ src/vuePdfNoSss.vue
@@ -1,24 +1,23 @@
 <style src="./annotationLayer.css"></style>
 <script>
+import componentFactory from "./componentFactory.js";
+import PdfjsWorker from "pdfjs-dist/build/pdf.worker.js";
+if (process.env.VUE_ENV !== "server") {
+  var pdfjsWrapper = require("./pdfjsWrapper.js").default;
+  var PDFJS = require("pdfjs-dist/es5/build/pdf.js");
+  if (
+    typeof window !== "undefined" &&
+    "Worker" in window &&
+    navigator.appVersion.indexOf("MSIE 10") === -1
+  ) {
+    // var PdfjsWorker = require('worker-loader!pdfjs-dist/es5/build/pdf.worker.js');
+    PDFJS.GlobalWorkerOptions.workerPort = new PdfjsWorker();
+  }
 
-	import componentFactory from './componentFactory.js'
+  var component = componentFactory(pdfjsWrapper(PDFJS));
+} else {
+  var component = componentFactory({});
+}
 
-	if ( process.env.VUE_ENV !== 'server' ) {
-
-		var pdfjsWrapper = require('./pdfjsWrapper.js').default;
-		var PDFJS = require('pdfjs-dist/es5/build/pdf.js');
-
-		if ( typeof window !== 'undefined' && 'Worker' in window && navigator.appVersion.indexOf('MSIE 10') === -1 ) {
-
-			var PdfjsWorker = require('worker-loader!pdfjs-dist/es5/build/pdf.worker.js');
-			PDFJS.GlobalWorkerOptions.workerPort = new PdfjsWorker();
-		}
-
-		var component = componentFactory(pdfjsWrapper(PDFJS));
-	} else {
-
-		var component = componentFactory({});
-	}
-
-	export default component;
+export default component;
 </script>
